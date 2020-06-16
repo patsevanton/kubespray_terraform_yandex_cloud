05d833fba72059a81780c7675dcc9318f6ec65a0
krylovia <krylovia@cloud-team.ru>
2020-06-16 11:55:13 +0300

    Fixes

--- cluster_install.sh
+++ cluster_install.sh
@@ -17,12 +17,6 @@ cd kubespray
 ansible-playbook -i inventory/mycluster/hosts.ini --become cluster.yml
 
 cd ../terraform
-printf "******************************************\n"
-printf "Load balancer public IP: "
-terraform output -json load_balancer_public_ip | jq -j ".[0]"
-printf "\n******************************************\n"
-
 MASTER_1_PRIVATE_IP=$(terraform output -json instance_group_masters_private_ips | jq -j ".[0]")
 MASTER_1_PUBLIC_IP=$(terraform output -json instance_group_masters_public_ips | jq -j ".[0]")
-
 sed -i -- "s/$MASTER_1_PRIVATE_IP/$MASTER_1_PUBLIC_IP/g" ../kubespray/inventory/mycluster/artifacts/admin.conf
