574858916d1cb7345dcc8e00fe077f5d8b50a5c5
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2021-08-22 19:34:26 +0600

    set yandex-cloud/yandex

--- terraform/k8s-cluster.tf
+++ terraform/k8s-cluster.tf
@@ -17,6 +17,15 @@ variable "yc_folder_id" {
 
 # Provider
 
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.61.0"
+    }
+  }
+}
+
 provider "yandex" {
   token     = var.yc_token
   cloud_id  = var.yc_cloud_id
