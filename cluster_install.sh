75fa80665d0477533abfa0344c7d18382495ebfd
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2021-08-24 12:50:39 +0600

    ../kubespray-2.14.2/inventory/mycluster/artifacts/admin.conf

--- cluster_install.sh
+++ cluster_install.sh
@@ -19,4 +19,4 @@ ansible-playbook -i inventory/mycluster/hosts.ini --become cluster.yml
 cd ../terraform
 MASTER_1_PRIVATE_IP=$(terraform output -json instance_group_masters_private_ips | jq -j ".[0]")
 MASTER_1_PUBLIC_IP=$(terraform output -json instance_group_masters_public_ips | jq -j ".[0]")
-sed -i -- "s/$MASTER_1_PRIVATE_IP/$MASTER_1_PUBLIC_IP/g" ../kubespray/inventory/mycluster/artifacts/admin.conf
+sed -i -- "s/$MASTER_1_PRIVATE_IP/$MASTER_1_PUBLIC_IP/g" ../kubespray-2.14.2/inventory/mycluster/artifacts/admin.conf
